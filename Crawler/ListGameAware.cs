4629b7da14fb84262b9dd727a1c16297a9117fd8
Chapelin <antbichon@gmail.com>
2015-03-25 12:44:33 +0100

    added a ListGameAware : generic List which handle game.Component (add/remove)

---
+++ Crawler/ListGameAware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crawler
+{
+    using Microsoft.Xna.Framework;
+
+    public class ListGameAware<T> : List<T> where T: GameComponent
+    {
+        private Game1 Game;
+        public ListGameAware(Game1 g)
+        {
+            this.Game = g;
+        }
+
+        public void Add(T obj)
+        {
+            base.Add(obj);
+            this.Game.Components.Add(obj);
+        }
+
+        public void Remove(T obj)
+        {
+            base.Remove(obj);
+            this.Game.Components.Remove(obj);
+        }
+
+        public void RemoveAt(int index)
+        {
+            var toRemove = base[index];
+            base.RemoveAt(index);
+            this.Game.Components.Remove(toRemove);
+        }
+
+        public void RemoveAll(Predicate<T> match)
+        {
+            var elementToRemove = this.FindAll(match);
+            base.RemoveAll(match);
+            foreach (var el in elementToRemove)
+            {
+                this.Game.Components.Remove(el);
+            }
+        }
+
+
+    }
+}
