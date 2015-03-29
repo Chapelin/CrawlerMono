45f4e0654223c96763d518caa2d31e3daf9c37fb
Chapelin <antbichon@gmail.com>
2015-03-29 13:37:18 +0200

    add Crwaler.Utils & test assembly

---
+++ Crawler.Utils.Tests/SimplePathCalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Xna.Framework;
+using Xunit;
+
+namespace Crawler.Utils.Tests
+{
+    public class SimplePathCalculatorTests
+    {
+
+        #region Fields
+
+        private SimplePathCalculator pathCalculator;
+
+        #endregion Fields
+
+        #region Ctor
+
+        public SimplePathCalculatorTests()
+        {
+            this.pathCalculator = new SimplePathCalculator();
+        }
+
+        #endregion Ctor
+
+        #region FindPath
+
+        [Fact]
+        public void SimplePathCalculator_WhenCallingFinPath_WithSameOriginAnTarget_ReturnEmptyList()
+        {
+            // Actors
+            var origin = new Vector2(5, 5);
+
+            // Actions
+            var result = this.pathCalculator.FindPath(origin, origin);
+
+            // Asserts
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        #endregion FindPath
+    }
+}
---
+++ Crawler.Utils/SimplePathCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Crawler.Utils
+{
+    public class SimplePathCalculator
+    {
+
+
+        public List<Vector2> FindPath(Vector2 origin, Vector2 target)
+        {
+
+            return null;
+        }
+    }
+}
