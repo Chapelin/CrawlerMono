2df05735e222c07c7b4742f17784adbeabeabfd2
Chapelin <antbichon@gmail.com>
2015-04-08 12:30:29 +0200

    added code for io position and path for each io generation

--- Crawler.Utils/MapGenerator/BasicMapGenerator.cs
+++ Crawler.Utils/MapGenerator/BasicMapGenerator.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+
 using Crawler.Utils.Random;
 using Microsoft.Xna.Framework;
 
@@ -16,21 +14,31 @@ namespace Crawler.Utils.MapGenerator
 
         public int RoomNumber;
 
+        private SimplePathCalculator spc;
         public Vector2 MapSize;
+
+        private RandomManager randomManager;
+
+        public BasicMapGenerator()
+        {
+            randomManager = new RandomManager();
+            spc = new SimplePathCalculator();
+        }
+
+
 
         public List<Room> GenerateRooms()
         {
-            var rng = new RandomManager();
             var listResult = new List<Room>();
             for (int i = 0; i < RoomNumber; i++)
             {
                 var cptRetry = 5;
                 while (cptRetry > 0)
                 {
-                    var nextSize = new Vector2(minRoomSize.X + rng.GetInt((int)(maxRoomSize.X - minRoomSize.X)),
-                        minRoomSize.Y + rng.GetInt((int)(maxRoomSize.Y - minRoomSize.Y)));
-                    var nextPos = new Vector2(rng.GetInt((int)(MapSize.X - nextSize.X - 1)),
-                        rng.GetInt((int)(MapSize.Y - nextSize.Y - 1)));
+                    var nextSize = new Vector2(minRoomSize.X + randomManager.GetInt((int)(maxRoomSize.X - minRoomSize.X)),
+                        minRoomSize.Y + randomManager.GetInt((int)(maxRoomSize.Y - minRoomSize.Y)));
+                    var nextPos = new Vector2(randomManager.GetInt((int)(MapSize.X - nextSize.X - 1)),
+                        randomManager.GetInt((int)(MapSize.Y - nextSize.Y - 1)));
 
                     var tentativeRoom = new Room()
                     {
@@ -55,12 +63,29 @@ namespace Crawler.Utils.MapGenerator
 
         public List<Room> GenerateIo(List<Room> lr )
         {
+            int maxIo = 4;
             for(int i=0; i<lr.Count; i++)
             {
                 var currentR = lr[i];
+                var currentnumberOfIo = randomManager.GetInt(maxIo-1)+1;
+                var listOfCell = Utilitaires.RectangleDelimitationCells(currentR.Setting);
+                for (int j = 0; j < currentnumberOfIo; j++)
+                {
+                    var nextIo = listOfCell[randomManager.GetInt(listOfCell.Count)];
+                    while(!currentR.IOs.Any(x=> (nextIo - x).Length()<=1))
+                    {
+                        nextIo = listOfCell[randomManager.GetInt(listOfCell.Count)];
+                    }
+                    currentR.IOs.Add(nextIo);
+                }
             }
 
             return lr;
+        }
+
+        public List<Vector2> GenerateBasicPath(Vector2 origin, Vector2 destination)
+        {
+            return spc.FindPath(origin, destination);
         }
 
     }
