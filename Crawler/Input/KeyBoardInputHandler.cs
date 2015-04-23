ea005b85f78ed2080359fc4eb68a706d0253f538
Chapelin <antbichon@gmail.com>
2015-04-23 20:43:07 +0200

    added previous/current keyboard state

--- Crawler/Input/KeyBoardInputHandler.cs
+++ Crawler/Input/KeyBoardInputHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Crawler.Input
+﻿using System.Collections.Generic;
+
+namespace Crawler.Input
 {
     using System;
     using System.Linq;
@@ -10,79 +12,67 @@
 
     public class KeyBoardInputHandler
     {
-
-        private int TimerResetValue = 30;
-        private int timer;
+        private KeyboardState previousKeyboardState;
+        private KeyboardState currentKeyboardState;
+        private List<Keys> newPressed;
 
         public KeyBoardInputHandler()
         {
-            this.ResetTimer();
         }
 
-        private void ResetTimer()
-        {
-            this.timer = this.TimerResetValue;
-        }
 
         public void HandleInput(LivingBeing lb)
         {
-            var ks = Keyboard.GetState();
-            var keys = ks.GetPressedKeys();
-            if (this.timer > 0)
-            {
-                this.timer--;
-            }
+            previousKeyboardState = currentKeyboardState;
+          currentKeyboardState = Keyboard.GetState();
+            newPressed = currentKeyboardState.GetPressedKeys().Except(previousKeyboardState.GetPressedKeys()).ToList();
 
-            if (keys.Any())
-            {
-                if (this.timer == 0)
-                {
-                    this.HandleKeyboardPlayerMovement(ks, lb);
 
-                    this.HandleKeyboardPlayerMenu( lb, keys);
-                }
+            if (newPressed.Any())
+            {
+           
+                    this.HandleKeyboardPlayerMovement (lb);
+
+                    this.HandleKeyboardPlayerMenu( lb);
+                
 
             }
 
         }
 
-        private void HandleKeyboardPlayerMenu(LivingBeing lb, Keys[] pressedKeys)
+        private void HandleKeyboardPlayerMenu(LivingBeing lb)
         {
-            if (pressedKeys.Contains(Keys.P))
+            if (newPressed.Contains(Keys.P))
             {
                 BlackBoard.CurrentMap.Pickup(lb);
-                this.ResetTimer();
             }
 
-            if (pressedKeys.Contains(Keys.I))
+            if (newPressed.Contains(Keys.I))
             {
-               BlackBoard.CurrentMap.ShowInventory(lb);
-                this.ResetTimer();
+                BlackBoard.CurrentMap.ShowInventory(lb);
             }
 
-            if (pressedKeys.Contains(Keys.D))
+            if (newPressed.Contains(Keys.D))
             {
                 BlackBoard.CurrentMap.DropFirstObject(lb);
-                this.ResetTimer();
-
             }
 
-            if (pressedKeys.Contains(Keys.Space))
+            if (newPressed.Contains(Keys.Space))
             {
                 BlackBoard.CurrentCamera.CenterOnCell(lb.positionCell);
             }
 
-            if (pressedKeys.Contains(Keys.L))
+            if (newPressed.Contains(Keys.L))
             {
                 Console.WriteLine("Action dispos : ");
                 var listAction = BlackBoard.CurrentMap.CellOnPosition(lb.positionCell).PossibleActions(lb);
                 foreach (var actionDoable in listAction)
                 {
-                    Console.WriteLine(actionDoable.Name + " "+ actionDoable.KeyBinding);
+                    Console.WriteLine(actionDoable.Name + " " + actionDoable.KeyBinding);
                 }
             }
 
-            if (pressedKeys.Contains(Keys.A))
+            if (newPressed.Contains(Keys.A))
             {
                 Console.WriteLine("Doing first action dispo");
                 var listAction = BlackBoard.CurrentMap.CellOnPosition(lb.positionCell).PossibleActions(lb);
@@ -92,7 +82,7 @@
                 }
             }
 
-            if (pressedKeys.Contains(Keys.E))
+            if (newPressed.Contains(Keys.E))
             {
                 Console.WriteLine("Trying to equipe first item");
                 var eq = lb.Inventory.FirstOrDefault(x => x.CanEquip(lb));
@@ -102,7 +92,7 @@
                 }
             }
 
-            if (pressedKeys.Contains(Keys.U))
+            if (newPressed.Contains(Keys.U))
             {
                 Console.WriteLine("Trying to unequipe first item");
                 var eq = lb.Inventory.FirstOrDefault(x => x.IsEquipped);
@@ -113,56 +103,52 @@
             }
         }
 
-        private void HandleKeyboardPlayerMovement(KeyboardState k, LivingBeing lb)
+        private void HandleKeyboardPlayerMovement(LivingBeing lb)
         {
             var targetCell = lb.positionCell;
-            if (k.IsKeyDown(Keys.NumPad2))
+            if (newPressed.Contains(Keys.NumPad2))
             {
                 targetCell.Y++;
             }
 
-            if (k.IsKeyDown(Keys.NumPad4))
+            if (newPressed.Contains(Keys.NumPad4))
             {
                 targetCell.X--;
             }
 
-            if (k.IsKeyDown(Keys.NumPad8))
+            if (newPressed.Contains(Keys.NumPad8))
             {
                 targetCell.Y--;
             }
 
-            if (k.IsKeyDown(Keys.NumPad6))
+            if (newPressed.Contains(Keys.NumPad6))
             {
                 targetCell.X++;
             }
 
-            if (k.IsKeyDown(Keys.NumPad9))
+            if (newPressed.Contains(Keys.NumPad9))
             {
                 targetCell += new Vector2(1, -1);
             }
 
-            if (k.IsKeyDown(Keys.NumPad7))
+            if (newPressed.Contains(Keys.NumPad7))
             {
                 targetCell += new Vector2(-1, -1);
             }
 
-            if (k.IsKeyDown(Keys.NumPad1))
+            if (newPressed.Contains(Keys.NumPad1))
             {
                 targetCell += new Vector2(-1, 1);
             }
 
-            if (k.IsKeyDown(Keys.NumPad3))
+            if (newPressed.Contains(Keys.NumPad3))
             {
                 targetCell += new Vector2(1, 1);
             }
 
             if (targetCell != lb.positionCell)
             {
-                var res = BlackBoard.CurrentMap.TryMoveLivingBeing(lb, targetCell);
-                if (res)
-                {
-                    this.ResetTimer();
-                }
+               BlackBoard.CurrentMap.TryMoveLivingBeing(lb, targetCell);
             }
         }
     }
