39d72b3d7363019e047852f7fb0d48d948ee4ad6
Chapelin <antbichon@gmail.com>
2015-04-23 21:58:27 +0200

    action dictionnary for general menu action

--- Crawler/Input/KeyBoardInputHandler.cs
+++ Crawler/Input/KeyBoardInputHandler.cs
@@ -12,10 +12,60 @@ namespace Crawler.Input
 
     public class KeyBoardInputHandler
     {
+        private delegate void ActionToDo(LivingBeing lb);
         private KeyboardState previousKeyboardState;
         private KeyboardState currentKeyboardState;
         private List<Keys> newPressed;
+        private Dictionary<Keys, ActionToDo> actionsBinded;
 
+        private void InitializeActions()
+        {
+            actionsBinded.Add(Keys.P, lb => BlackBoard.CurrentMap.Pickup(lb));
+            actionsBinded.Add(Keys.I, lb => BlackBoard.CurrentMap.ShowInventory(lb));
+            actionsBinded.Add(Keys.D, lb => BlackBoard.CurrentMap.DropFirstObject(lb));
+            actionsBinded.Add(Keys.Space, lb => BlackBoard.CurrentCamera.CenterOnCell(lb.positionCell));
+            actionsBinded.Add(Keys.L, lb =>
+            {
+                Console.WriteLine("Action dispos : ");
+                var listAction = BlackBoard.CurrentMap.CellOnPosition(lb.positionCell).PossibleActions(lb);
+                foreach (var actionDoable in listAction)
+                {
+                    Console.WriteLine(actionDoable.Name + " " + actionDoable.KeyBinding);
+                }
+            });
+
+            actionsBinded.Add(Keys.A, lb =>
+            {
+                Console.WriteLine("Doing first action dispo");
+                var listAction = BlackBoard.CurrentMap.CellOnPosition(lb.positionCell).PossibleActions(lb);
+                if (listAction.Any())
+                {
+                    listAction.First().Activity.Invoke();
+                }
+            });
+
+            actionsBinded.Add(Keys.E, lb =>
+            {
+                Console.WriteLine("Trying to equipe first item");
+                var eq = lb.Inventory.FirstOrDefault(x => x.CanEquip(lb));
+                if (eq != null)
+                {
+                    eq.Equip(lb);
+                }
+            });
+
+            actionsBinded.Add(Keys.U, lb =>
+            {
+                Console.WriteLine("Trying to unequipe first item");
+                var eq = lb.Inventory.FirstOrDefault(x => x.IsEquipped);
+                if (eq != null)
+                {
+                    eq.UnEquip(lb);
+                }
+            });
+
+
+        }
 
         public void HandleInput(LivingBeing lb)
         {
@@ -34,64 +84,10 @@ namespace Crawler.Input
 
         private void HandleKeyboardPlayerMenu(LivingBeing lb)
         {
-            if (newPressed.Contains(Keys.P))
+            if (actionsBinded.Any(x => newPressed.Contains(x.Key)))
             {
-                BlackBoard.CurrentMap.Pickup(lb);
-            }
-
-            if (newPressed.Contains(Keys.I))
-            {
-                BlackBoard.CurrentMap.ShowInventory(lb);
-            }
-
-            if (newPressed.Contains(Keys.D))
-            {
-                BlackBoard.CurrentMap.DropFirstObject(lb);
-            }
-
-            if (newPressed.Contains(Keys.Space))
-            {
-                BlackBoard.CurrentCamera.CenterOnCell(lb.positionCell);
-            }
-
-            if (newPressed.Contains(Keys.L))
-            {
-                Console.WriteLine("Action dispos : ");
-                var listAction = BlackBoard.CurrentMap.CellOnPosition(lb.positionCell).PossibleActions(lb);
-                foreach (var actionDoable in listAction)
-                {
-                    Console.WriteLine(actionDoable.Name + " " + actionDoable.KeyBinding);
-                }
-            }
-
-            if (newPressed.Contains(Keys.A))
-            {
-                Console.WriteLine("Doing first action dispo");
-                var listAction = BlackBoard.CurrentMap.CellOnPosition(lb.positionCell).PossibleActions(lb);
-                if (listAction.Any())
-                {
-                    listAction.First().Activity.Invoke();
-                }
-            }
-
-            if (newPressed.Contains(Keys.E))
-            {
-                Console.WriteLine("Trying to equipe first item");
-                var eq = lb.Inventory.FirstOrDefault(x => x.CanEquip(lb));
-                if (eq != null)
-                {
-                    eq.Equip(lb);
-                }
-            }
-
-            if (newPressed.Contains(Keys.U))
-            {
-                Console.WriteLine("Trying to unequipe first item");
-                var eq = lb.Inventory.FirstOrDefault(x => x.IsEquipped);
-                if (eq != null)
-                {
-                    eq.UnEquip(lb);
-                }
+                var action = this.actionsBinded.FirstOrDefault(x => newPressed.Contains(x.Key)).Value;
+                action(lb);
             }
         }
 
@@ -143,6 +139,12 @@ namespace Crawler.Input
             {
                 BlackBoard.CurrentMap.TryMoveLivingBeingOfVector(lb, depVector);
             }
+        }
+
+        public KeyBoardInputHandler()
+        {
+            this.actionsBinded = new Dictionary<Keys, ActionToDo>();
+            InitializeActions();
         }
     }
 }
